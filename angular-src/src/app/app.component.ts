9793d5703f055e14da28997668ec0eda6fe53eb2
王斌 <253498229@qq.com>
2025-01-12 22:00:21 +0800

    添加api支持

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './shared/shared.module';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { app } from '@neutralinojs/lib';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,14 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCollapsed = false;
+
+  ngOnInit(): void {
+    app.getConfig().then(r => {
+      console.log('getConfig r', r);
+    }).catch(e => {
+      console.log('getConfig e', e);
+    });
+  }
 }
